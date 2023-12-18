27966e14f302c8ec86e4cd19bf8e443ab55713c4
mirajsankdecha <mirajsankdecha@gmail.com>
2023-12-19 00:40:54 +0530

    TextSizer Activity Created

---
+++ Unit1/Toast/app/src/main/java/com/example/toast/TextSize.java
@@ -0,0 +1,29 @@
+package com.example.toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.view.View;
+
+public class TextSize extends AppCompatActivity {
+
+    private float textSize;
+    private TextView t1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_size);
+        t1 = findViewById(R.id.lblname);
+        textSize = t1.getTextSize();
+    }
+    public void Inc(View view)
+    {
+        textSize+=5;
+        t1.setTextSize(textSize);
+    }
+    public void  Dec(View view)
+    {
+        textSize-=5;
+        t1.setTextSize(textSize);
+    }
+}
