3447b6c863f4aae867c3cde12e6e8ca5fd9d64f6
mirajsankdecha <mirajsankdecha>
2023-12-21 08:43:54 +0530

    Checkbox,RadioButton,Label

---
+++ Unit1/Toast/app/src/main/java/com/example/toast/Radio_Checkbox_Label.java
@@ -0,0 +1,14 @@
+package com.example.toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Radio_Checkbox_Label extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_radio_checkbox_label);
+    }
+}
