95d691515a5233fd1cd451d161b7e7c8ba2bb6f2
mirajsankdecha <mirajsankdecha@gmail.com>
2024-01-02 09:11:12 +0530

    Imp and Exp Intent 2 created

--- Unit1/Toast/app/src/main/java/com/example/toast/Imp_and_Exp_Intent.java
+++ Unit1/Toast/app/src/main/java/com/example/toast/Imp_and_Exp_Intent.java
@@ -7,6 +7,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class Imp_and_Exp_Intent extends AppCompatActivity {
 
@@ -16,11 +17,20 @@ public class Imp_and_Exp_Intent extends AppCompatActivity {
         setContentView(R.layout.activity_imp_and_exp_intent);
         Button b1 = findViewById(R.id.btnri);
         Button b2 = findViewById(R.id.btnci);
+        EditText e1 = findViewById(R.id.tvnm);
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(Intent.ACTION_VIEW);
                 i.setData(Uri.parse("https://portfolio-master-sable-seven.vercel.app/"));
+                startActivity(i);
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),Imp_and_Exp_Intent_2.class);
+                String name = e1.getText().toString();
                 startActivity(i);
             }
         });
---
+++ Unit1/Toast/app/src/main/java/com/example/toast/Imp_and_Exp_Intent_2.java
@@ -0,0 +1,14 @@
+package com.example.toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Imp_and_Exp_Intent_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_imp_and_exp_intent2);
+    }
+}
