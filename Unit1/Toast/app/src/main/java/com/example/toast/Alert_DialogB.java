d490918cbc9f3c16e29e9d17ef8ddfda473a3f1f
mirajsankdecha <mirajsankdecha@gmail.com>
2024-01-09 08:05:09 +0530

    Alert Activity XML and Activity Created

---
+++ Unit1/Toast/app/src/main/java/com/example/toast/Alert_DialogB.java
@@ -0,0 +1,14 @@
+package com.example.toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Alert_DialogB extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alert_dialog_b);
+    }
+}
