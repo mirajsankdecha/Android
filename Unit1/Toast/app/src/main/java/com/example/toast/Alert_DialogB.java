2f612836f974d9b552c8e3c15ed2807bae0937a4
mirajsankdecha <mirajsankdecha@gmail.com>
2024-01-09 08:32:30 +0530

    Alert Activity Complate

--- Unit1/Toast/app/src/main/java/com/example/toast/Alert_DialogB.java
+++ Unit1/Toast/app/src/main/java/com/example/toast/Alert_DialogB.java
@@ -1,8 +1,13 @@
 package com.example.toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Alert_DialogB extends AppCompatActivity {
 
@@ -10,5 +15,28 @@ public class Alert_DialogB extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alert_dialog_b);
+        Button b1 = findViewById(R.id.btn1);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(Alert_DialogB.this);
+                builder.setTitle("Delete Alert!")
+                        .setMessage("Are you sure you want to Delete ?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Toast.makeText(Alert_DialogB.this, "Chalega", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Toast.makeText(Alert_DialogB.this, "Noooo", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .create()
+                        .show();
+            }
+        });
     }
 }
