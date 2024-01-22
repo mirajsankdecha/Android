9dc9571f1caa59b75a47e8834a65d73eff870252
mirajsankdecha <mirajsankdecha@gmail.com>
2024-01-22 19:12:46 +0530

    Assignment 2 first program second activity created

--- Assignment2/app/src/main/java/com/example/assignment2/resultpage1.java
+++ Assignment2/app/src/main/java/com/example/assignment2/resultpage1.java
@@ -1,7 +1,13 @@
 package com.example.assignment2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class resultpage1 extends AppCompatActivity {
 
@@ -9,5 +15,29 @@ public class resultpage1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultpage1);
+        TextView nm = findViewById(R.id.name);
+        TextView en = findViewById(R.id.enrollnum);
+        TextView py = findViewById(R.id.py);
+        TextView mern = findViewById(R.id.mern);
+        TextView ad = findViewById(R.id.adroid);
+        TextView asp = findViewById(R.id.asp);
+        TextView lvl = findViewById(R.id.laravel);
+        TextView cs = findViewById(R.id.cs);
+        Button btn = findViewById(R.id.btnsubmit);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),resultpage2.class);
+                String nam = nm.getText().toString();
+                String enoo = en.getText().toString();
+                int pyy = Integer.parseInt(py.getText().toString());
+                int mernn = Integer.parseInt(mern.getText().toString());
+                int add = Integer.parseInt(ad.getText().toString());
+                int aspp = Integer.parseInt(asp.getText().toString());
+                int lvll = Integer.parseInt(lvl.getText().toString());
+                int css = Integer.parseInt(cs.getText().toString());
+                
+            }
+        });
     }
 }
