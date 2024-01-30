1dd5a79800263ab36d3435a16807f1d4dfaf80c3
mirajsankdecha <mirajsankdecha@gmail.com>
2024-01-30 08:41:56 +0530

    Unit 3 Started

---
+++ Unit1/Toast/app/src/main/java/com/example/toast/sharedprefrence.java
@@ -0,0 +1,20 @@
+package com.example.toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+public class sharedprefrence extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sharedprefrence);
+        SharedPreferences sp = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putString("Name","Miraj Sankdecha");
+        editor.putInt("Enrollment",210801300);
+        editor.commit();
+    }
+}
