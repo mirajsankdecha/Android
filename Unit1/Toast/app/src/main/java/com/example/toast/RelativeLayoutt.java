959cecab29177179a1821bb37594120dfa303eab
mirajsankdecha <mirajsankdecha@gmail.com>
2024-02-13 00:24:33 +0530

    Relative Layout program compeleted

---
+++ Unit1/Toast/app/src/main/java/com/example/toast/RelativeLayoutt.java
@@ -0,0 +1,14 @@
+package com.example.toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RelativeLayoutt extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relative_layoutt);
+    }
+}
