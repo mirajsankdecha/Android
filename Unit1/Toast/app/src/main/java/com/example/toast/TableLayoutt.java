b777d4609d64af3fc6b2f7cf4f5d51b6c976f009
mirajsankdecha <mirajsankdecha@gmail.com>
2024-02-13 00:31:03 +0530

    Table Layout program compeleted

---
+++ Unit1/Toast/app/src/main/java/com/example/toast/TableLayoutt.java
@@ -0,0 +1,14 @@
+package com.example.toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TableLayoutt extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_table_layoutt);
+    }
+}
