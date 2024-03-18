b2857a29635941dec269d37aa61538b8b58d9c41
mirajsankdecha <mirajsankdecha@gmail.com>
2024-03-18 22:01:59 +0530

    DB Helper class create

--- Unit1/Toast/app/src/main/java/com/example/toast/DBHelper.java
+++ Unit1/Toast/app/src/main/java/com/example/toast/DBHelper.java
@@ -1,23 +1,5 @@
 package com.example.toast;
 
-import android.content.Context;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
+public class DBHelper {
 
-import androidx.annotation.Nullable;
-
-public class DBHelper extends SQLiteOpenHelper {
-    public DBHelper(@Nullable Context context) {
-        super(context, "mydb", null, 1);
-    }
-
-    @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
-    }
 }
--- Unit1/Toast/app/src/main/java/com/example/toast/sqllite_db.java
+++ Unit1/Toast/app/src/main/java/com/example/toast/sqllite_db.java
@@ -3,6 +3,9 @@ package com.example.toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class sqllite_db extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class sqllite_db extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sqllite_db);
+        Button insert  = findViewById(R.id.btnins);
+        Button update = findViewById(R.id.btnupdate);
+        Button delete = findViewById(R.id.btndelete);
+        Button view  = findViewById(R.id.btnview);
+        EditText tv1 = findViewById(R.id.tbid);
+        EditText tv2 = findViewById(R.id.tbname);
+        TextView tv0 = findViewById(R.id.lbldisp);
+
     }
 }
